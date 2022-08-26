e14c5dcd469b52e46de5f1daaaf5865fde648733
gle-bellier <georges.lebellier@gmail.com>
2022-08-26 16:42:01 +0200

    Add test of efficient unet output shape

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,6 +1,6 @@
 import torch
 import pytest
-from typing import Tuple
+from typing import Tuple, List
 
 
 @pytest.mark.parametrize("input_shape, stride, expected",
@@ -58,3 +58,31 @@ def test_up_block_shape(input_shape: Tuple[int], stride: int,
     noise_scale = torch.randn(batch_size, 512)
 
     assert up(x, skip, noise_scale).shape == expected
+
+
+@pytest.mark.parametrize(
+    "input_shape, channels, strides, num_resnets",
+    [((13, 2, 1024), [2, 4, 8, 16], [2, 2, 4], [1, 1, 3]),
+     ((13, 2, 1024), [2, 4, 8, 8, 16], [2, 2, 4, 8], [1, 1, 3, 5])])
+def test_efficient_unet_shape(input_shape: Tuple[int], channels: List[int],
+                              strides: List[int],
+                              num_resnets: List[int]) -> None:
+    """Test shape of output of upsampling block.
+
+    Args:
+        input_shape (Tuple[int]): input shape (B C_in L_in).
+        stride (int): stride of the up block.
+        expected (Tuple[int]): expected output shape (B C_out L_out).
+    """
+
+    from models.efficient_unet import EfficientUnet
+
+    batch_size = input_shape[0]
+    sample_length = input_shape[-1]
+
+    unet = EfficientUnet(sample_length, channels, strides, num_resnets)
+
+    x = torch.randn(input_shape)
+    noise_scale = torch.randn(batch_size, 1)
+
+    assert unet(x, noise_scale).shape == input_shape
