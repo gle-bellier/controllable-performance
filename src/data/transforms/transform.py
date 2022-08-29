053ebb607a88fde0dcba4cd7fb5447571345f960
gle-bellier <georges.lebellier@gmail.com>
2022-08-29 18:44:49 +0200

    Minor change

--- src/data/transforms/transform.py
+++ src/data/transforms/transform.py
@@ -23,3 +23,17 @@ class ConditionTransform:
             torch.Tensor: modified condition.
         """
         raise NotImplementedError
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply transform to a input tensor.
+
+        Args:
+            x (torch.Tensor): conditioning tensor of shape
+            (B C L).
+            
+            NotImplementedError: error if not implemented.
+
+        Returns:
+            torch.Tensor: modified condition.
+        """
+        return self.transform(x)
