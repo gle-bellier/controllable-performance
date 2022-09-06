c5d3e686f0325d0cb7b6c3ca86ae27355f805439
gle-bellier <georges.lebellier@gmail.com>
2022-09-06 13:20:46 +0200

    Add conditionnal and unconditionnal training

--- src/train/diffusion.py
+++ src/train/diffusion.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from typing import Any, OrderedDict, Tuple, List
 import pytorch_lightning as pl
 
@@ -17,8 +15,20 @@ class Diffusion(pl.LightningModule):
                  data_processor: ContoursProcessor,
                  transform: ConditionTransform,
                  sampler: Sampler,
-                 sampling_rate: float,
+                 learning_rate: float,
                  sample_length=1024) -> None:
+        """Initialize denoising diffusion model.
+
+        Args:
+            data_processor (ContoursProcessor): dataprocessor used to process 
+            the contours.
+            transform (ConditionTransform): transformation applied to the condition
+            signal.
+            sampler (Sampler): denoising diffusion sampler.
+            learning_rate (float): training learning rate. 
+            sample_length (int, optional): length L of contours of shape (B C L).
+            Defaults to 1024.
+        """
         super().__init__()
 
         self.save_hyperparameters()
@@ -31,19 +41,20 @@ class Diffusion(pl.LightningModule):
         self.model = self.sampler.model
         self.sde = self.sampler.sde
 
-        self.sampling_rate = sampling_rate
+        self.learning_rate = learning_rate
         self.sample_length = sample_length
 
         self.train_step_idx = 0
         self.val_step_idx = 0
 
-    def configure_optimizers(self) -> torch.optim.Optimizer:
+    def configure_optimizers(self) -> Tuple[torch.optim.Optimizer]:
         """Configure optimizers.
         Returns:
             torch.optim.Optimizer: optimizer.
+            torch.optim._LRScheduler: scheduler.
         """
         optimizer = torch.optim.AdamW(self.parameters(),
-                                      lr=self.sampling_rate,
+                                      lr=self.learning_rate,
                                       weight_decay=1e-3)
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
                                                     step_size=150000,
@@ -51,34 +62,32 @@ class Diffusion(pl.LightningModule):
         return [optimizer], [scheduler]
 
     def forward(self, contours: torch.Tensor) -> torch.Tensor:
+        """Compute denoising diffusion model forward pass.
 
-        # condition = transformed (blurred/filtered) contours
-        condition = self.T(contours)
+        Args:
+            contours (torch.Tensor): input conditioning contours
+            of shape (B C L).
 
-        # preprocess the data
-        contours = self.P(contours)
-        condition = self.P(condition)
+        Raises:
+            NotImplementedError: error raised if not implemented.
 
-        # create time batch
-        batch_size = contours.shape[0]
-        t = torch.rand(batch_size, 1, 1, device=contours.device)
+        Returns:
+            torch.Tensor: loss.
+        """
 
-        # ensure t in [t_min, t_max)
-        t = (self.sde.t_max - self.sde.t_min) * t + self.sde.t_min
-
-        # sample noise z
-        z = torch.randn_like(contours, device=contours.device)
-
-        # noise contours and predict injected noise
-        contours_t = self.sde.perturb(contours, t, z)
-        noise_scale = self.sde.sigma(t).squeeze(-1)
-        z_hat = self.model(contours_t, condition, noise_scale)
-
-        loss = F.mse_loss(z, z_hat)
-
-        return loss
+        raise NotImplementedError
 
     def sample(self, condition: torch.Tensor) -> Tuple[torch.Tensor]:
+        """Sample new contours.
+
+        Args:
+            condition (torch.Tensor): conditioning contours
+            of shape (B C L).
+
+        Returns:
+            Tuple[torch.Tensor]: sampled contours of shape (B C L)
+            and audio generated of shape (B L*sampling_rate).
+        """
 
         # apply preprocessing
         condition = self.P(condition)
@@ -93,9 +102,20 @@ class Diffusion(pl.LightningModule):
 
         return sample, audio
 
-    def generate(self, n_samples: int) -> torch.Tensor:
+    def generate(self, n_samples: int) -> dict:
+        """Generate contours from MIDI and expressive contours.
 
-        self.model.eval()
+        Args:
+            n_samples (int): number of contours to generate in 
+            the two categories (from MIDI and from expressive 
+            contours).
+
+        Returns:
+            dict: {
+            "contours": (midi_contours, expressive_contours),
+            "audio": (midi_audio, expressive_audio)
+        } 
+        """
 
         # get midi contours as condition
         midi = self.get_midi(n_samples, self.sample_length)
@@ -117,6 +137,16 @@ class Diffusion(pl.LightningModule):
 
     def training_step(self, contours: torch.Tensor,
                       batch_idx: int) -> OrderedDict:
+        """Compute training step.
+
+        Args:
+            contours (torch.Tensor): expressive contours
+            of shape (B C L) from the training dataset.
+            batch_idx (int): index of the batch.
+
+        Returns:
+            OrderedDict: {"loss"}
+        """
 
         loss = self(contours)
         # log the training loss
@@ -128,6 +158,16 @@ class Diffusion(pl.LightningModule):
 
     def validation_step(self, contours: torch.Tensor,
                         batch_idx: int) -> OrderedDict:
+        """Compute validation step.
+
+        Args:
+            contours (torch.Tensor): expressive contours
+            of shape (B C L) from the validation dataset.
+            batch_idx (int): index of the batch.
+
+        Returns:
+            OrderedDict: {"loss"}
+        """
 
         loss = self(contours)
         # log the training loss
@@ -138,6 +178,12 @@ class Diffusion(pl.LightningModule):
         return {"loss": loss, "contours": contours}
 
     def validation_epoch_end(self, batch_parts: List[dict]) -> None:
+        """Plot and listen to generated contours at the end of the validation 
+        step.
+
+        Args:
+            batch_parts (List[dict]): results of the validation step.
+        """
 
         # select last item expressive contours
         contours = batch_parts[-1]["contours"]
