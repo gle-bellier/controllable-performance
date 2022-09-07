2dbace354b103e7451ca64f9ddd05dc33b8c37e2
gle-bellier <georges.lebellier@gmail.com>
2022-09-07 11:59:43 +0200

    Refactor model code

--- src/models/efficient_unet.py
+++ src/models/efficient_unet.py
@@ -6,10 +6,11 @@ import torch.nn as nn
 from .blocks import RFFBlock, UpBlock, DownBlock
 
 
-class UnconditionalUnet(nn.Module):
+class EfficientUnet(nn.Module):
 
     def __init__(self, sample_length: int, channels: List[int],
-                 strides: List[int], num_resnets: List[int]) -> None:
+                 strides: List[int], num_resnets: List[int],
+                 conditional: bool) -> None:
         """Initialize EfficientUnet. Architecture inspired by the efficient unet
         used in Imagen article. Does not include attention layers.
 
@@ -19,13 +20,15 @@ class UnconditionalUnet(nn.Module):
             strides (List[int]): list of the strides of the convolution blocks.
             num_resnets (List[int]): list of the number of resnets in each of the 
             down/upsampling block (symetrie).
+            conditional (bool): if set to True then conditioning signal is taken into
+            account in addition of the noise scale embedding.
         """
 
-        super(UnconditionalUnet, self).__init__()
+        super(EfficientUnet, self).__init__()
 
         h_dims = [32, 128, 512]
-        lengths_sample = [sample_length // cs for cs in np.cumprod(strides)]
-        self.embedding = RFFBlock(input_dim=1,
+        input_lengths = [sample_length // cs for cs in np.cumprod(strides)]
+        self.noise_rff = RFFBlock(input_dim=1,
                                   rff_dim=32,
                                   mlp_hidden_dims=h_dims)
 
@@ -33,21 +36,29 @@ class UnconditionalUnet(nn.Module):
             nn.Conv1d(channels[0], channels[0], kernel_size=1, padding=0),
             nn.SiLU())
         self.dwn_blocks = nn.ModuleList([
-            DownBlock(sample_length, in_c, out_c, stride, num_resnets=n)
-            for sample_length, in_c, out_c, stride, n in zip(
-                lengths_sample, channels[:-1], channels[1:], strides,
+            DownBlock(sample_length=sample_length,
+                      input_length=input_length,
+                      in_c=in_c,
+                      out_c=out_c,
+                      stride=stride,
+                      num_resnets=n,
+                      conditional=conditional)
+            for input_length, in_c, out_c, stride, n in zip(
+                input_lengths, channels[:-1], channels[1:], strides,
                 num_resnets)
         ])
         skip_co = [True] * (len(strides) - 1) + [False]
         self.up_blocks = nn.ModuleList([
-            UpBlock(sample_length,
-                    in_c,
-                    out_c,
-                    stride,
+            UpBlock(sample_length=sample_length,
+                    input_length=input_length,
+                    in_c=in_c,
+                    out_c=out_c,
+                    stride=stride,
                     num_resnets=n,
+                    conditional=conditional,
                     skip_co=sc)
-            for sample_length, in_c, out_c, stride, n, sc in zip(
-                lengths_sample[::-1], channels[-1:0:-1], channels[-2::-1],
+            for input_length, in_c, out_c, stride, n, sc in zip(
+                input_lengths[::-1], channels[-1:0:-1], channels[-2::-1],
                 strides[::-1], num_resnets[::-1], skip_co)
         ])
         self.post = nn.Conv1d(channels[0],
@@ -71,16 +82,16 @@ class UnconditionalUnet(nn.Module):
             and L=sample_length.
         """
 
-        noise_scale = self.embedding(noise_scale)
+        noise_scale = self.noise_rff(noise_scale)
         x = self.pre(x)
         dwn_outs = []
         # downsampling
         for dwn in self.dwn_blocks:
-            x = dwn(x, noise_scale)
+            x = dwn(x, condition, noise_scale)
             dwn_outs += [x]
 
         # upsampling
         for up, skip in zip(self.up_blocks, dwn_outs[::-1]):
-            x = up(x, skip, noise_scale)
+            x = up(x, condition, noise_scale, skip)
 
         return self.post(x)
