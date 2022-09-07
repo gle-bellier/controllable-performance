6c8aa8ba91bdd3aef059cf7f38f539afc65f431e
gle-bellier <georges.lebellier@gmail.com>
2022-09-07 11:46:27 +0200

    Update upsampling block and its test

--- src/models/blocks/up_block.py
+++ src/models/blocks/up_block.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from models.blocks.conditional_emb import ConditionalEmbs
+from models.blocks.conditional_emb import ConditionEmbedder
 from models.blocks.resnet_block import ResNetBlock
 import numpy as np
 
@@ -10,35 +10,46 @@ class UpBlock(nn.Module):
 
     def __init__(self,
                  sample_length: int,
+                 input_length: int,
                  in_c: int,
                  out_c: int,
                  stride: int,
                  num_resnets: int,
+                 conditional: bool,
                  skip_co=True) -> None:
         """Initialize UpBlock.
 
         Args:
-            sample_length (int): length L of the input of shape (B C L).
+            sample_length (int): length L' of the sample of shape (B 2 L').
+            input_length (int): length L of the input of shape (B C L).
             in_c (int): number of input channels in the convolution.
             out_c (int): number of output channels in the convolution.
             stride (int): stride of the convolution.
             num_resnets (int): number of resnet in the upsampling block.
+            conditional (bool): if set to True then the conditoning signal is 
+            taken into account in the ConditionEmbedder in addition to the 
+            noise_scale.
             skip_co (bool): if block takes skip connection ouput as input.
             Default to True.
         """
         super(UpBlock, self).__init__()
         self.skip_co = skip_co
         self.num_resnets = num_resnets
-        self.noise_emb = ConditionalEmbs(sample_length=sample_length,
-                                         in_c=in_c)
-        self.residual = nn.Sequential(
-            *[ResNetBlock(in_c, sample_length) for _ in range(num_resnets)])
-        self.up = nn.ConvTranspose1d(in_channels=in_c,
-                                     out_channels=out_c,
-                                     kernel_size=3,
-                                     stride=stride,
-                                     padding=self.get_padding(3, stride, 1),
-                                     output_padding=stride - 1)
+        self.embedder = ConditionEmbedder(sample_length=sample_length,
+                                          input_length=input_length,
+                                          in_c=in_c,
+                                          conditional=conditional)
+        self.residual = nn.Sequential(*[
+            ResNetBlock(input_length=input_length, in_c=in_c)
+            for _ in range(num_resnets)
+        ])
+        self.up = nn.Sequential(
+            nn.ConvTranspose1d(in_channels=in_c,
+                               out_channels=out_c,
+                               kernel_size=3,
+                               stride=stride,
+                               padding=self.get_padding(3, stride, 1),
+                               output_padding=stride - 1), nn.SiLU())
 
     def get_padding(self, kernel_size: int, stride: int, dilation: int) -> int:
         """Return size of the padding needed.
@@ -53,16 +64,17 @@ class UpBlock(nn.Module):
         full_kernel = (kernel_size - 1) * dilation + 1
         return full_kernel // 2
 
-    def forward(self, x: torch.Tensor, skip: torch.Tensor,
-                noise_scale: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, condition: torch.Tensor,
+                noise_scale: torch.Tensor, skip: torch.Tensor) -> torch.Tensor:
         """Compute pass forward.
 
         Args:
             x (torch.Tensor): input tensor of shape (B C L).
-            skip (torch.Tensor): input tensor from skip connection
-            of shape (B C L).
+            condition (torch.Tensor): condition tensor of shape (B 2 sample_length).
             noise_scale (torch.Tensor): noise scale embedding of shape
             (B N=512).
+            skip (torch.Tensor): input tensor from skip connection
+            of shape (B C L).
 
         Returns:
             torch.Tensor: output tensor of shape (B C' L').
@@ -71,6 +83,6 @@ class UpBlock(nn.Module):
             if self.num_resnets > 3:
                 skip = skip / np.sqrt(2)
             x = x + skip
-        x = self.noise_emb(x, noise_scale)
+        x = self.embedder(x, condition, noise_scale)
         x = self.residual(x)
         return self.up(x)
---
+++ tests/test_updown_shapes.py
@@ -0,0 +1,138 @@
+import torch
+import pytest
+from typing import Tuple, List
+
+
+@pytest.mark.parametrize("input_shape, stride, expected",
+                         [((13, 2, 1024), 2, (13, 16, 512)),
+                          ((13, 2, 1024), 1, (13, 16, 1024)),
+                          ((13, 2, 1024), 4, (13, 4, 256))])
+def test_uncond_down_block_shape(input_shape: Tuple[int], stride: int,
+                                 expected: Tuple[int]) -> None:
+    """Test shape of output of upsampling block.
+
+    Args:
+        input_shape (Tuple[int]): input shape (B C_in L_in).
+        stride (int): stride of the up block.
+        expected (Tuple[int]): expected output shape (B C_out L_out).
+    """
+
+    from models.blocks.down_block import DownBlock
+
+    batch_size = input_shape[0]
+    in_c, out_c = input_shape[1], expected[1]
+    sample_length = input_shape[-1]
+    input_length = 1024
+
+    down = DownBlock(sample_length, input_length, in_c, out_c, stride, 2,
+                     False)
+
+    x = torch.randn(input_shape)
+    condition = torch.randn(batch_size, 2, 1024)
+    noise_scale = torch.randn(batch_size, 512)
+
+    assert down(x, condition, noise_scale).shape == expected
+
+
+@pytest.mark.parametrize("input_shape, stride, expected",
+                         [((13, 2, 1024), 2, (13, 16, 512)),
+                          ((13, 2, 1024), 1, (13, 16, 1024)),
+                          ((13, 2, 1024), 4, (13, 4, 256))])
+def test_cond_down_block_shape(input_shape: Tuple[int], stride: int,
+                               expected: Tuple[int]) -> None:
+    """Test shape of output of upsampling block.
+
+    Args:
+        input_shape (Tuple[int]): input shape (B C_in L_in).
+        stride (int): stride of the up block.
+        expected (Tuple[int]): expected output shape (B C_out L_out).
+    """
+
+    from models.blocks.down_block import DownBlock
+
+    batch_size = input_shape[0]
+    in_c, out_c = input_shape[1], expected[1]
+    sample_length = input_shape[-1]
+    input_length = 1024
+
+    down = DownBlock(sample_length, input_length, in_c, out_c, stride, 2, True)
+
+    x = torch.randn(input_shape)
+    condition = torch.randn(batch_size, 2, 1024)
+    noise_scale = torch.randn(batch_size, 512)
+
+    assert down(x, condition, noise_scale).shape == expected
+
+
+@pytest.mark.parametrize("input_shape, stride, expected",
+                         [((13, 2, 1024), 2, (13, 16, 2048)),
+                          ((13, 2, 512), 2, (13, 16, 1024)),
+                          ((13, 2, 1024), 4, (13, 4, 4096))])
+def test_uncond_up_block_shape(input_shape: Tuple[int], stride: int,
+                               expected: Tuple[int]) -> None:
+    """Test shape of output of upsampling block.
+
+    Args:
+        input_shape (Tuple[int]): input shape (B C_in L_in).
+        stride (int): stride of the up block.
+        expected (Tuple[int]): expected output shape (B C_out L_out).
+    """
+
+    from models.blocks.up_block import UpBlock
+
+    batch_size = input_shape[0]
+    in_c, out_c = input_shape[1], expected[1]
+    input_length = input_shape[-1]
+
+    x = torch.randn(input_shape)
+    condition = torch.randn(batch_size, 2, 1024)
+    skip = torch.randn(input_shape)
+    noise_scale = torch.randn(batch_size, 512)
+
+    up = UpBlock(sample_length=1024,
+                 input_length=input_length,
+                 in_c=in_c,
+                 out_c=out_c,
+                 stride=stride,
+                 num_resnets=2,
+                 conditional=False,
+                 skip_co=True)
+
+    assert up(x, condition, noise_scale, skip).shape == expected
+
+
+@pytest.mark.parametrize("input_shape, stride, expected",
+                         [((13, 2, 1024), 2, (13, 16, 2048)),
+                          ((13, 2, 512), 2, (13, 16, 1024)),
+                          ((13, 2, 1024), 4, (13, 4, 4096))])
+def test_cond_up_block_shape(input_shape: Tuple[int], stride: int,
+                             expected: Tuple[int]) -> None:
+    """Test shape of output of upsampling block.
+
+    Args:
+        input_shape (Tuple[int]): input shape (B C_in L_in).
+        stride (int): stride of the up block.
+        expected (Tuple[int]): expected output shape (B C_out L_out).
+    """
+
+    from models.blocks.up_block import UpBlock
+
+    batch_size = input_shape[0]
+    in_c, out_c = input_shape[1], expected[1]
+    input_length = input_shape[-1]
+
+    x = torch.randn(input_shape)
+    condition = torch.randn(batch_size, 2, 1024)
+    skip = torch.randn(input_shape)
+    noise_scale = torch.randn(batch_size, 512)
+
+    up = UpBlock(sample_length=1024,
+                 input_length=input_length,
+                 in_c=in_c,
+                 out_c=out_c,
+                 stride=stride,
+                 num_resnets=2,
+                 conditional=True,
+                 skip_co=True)
+
+    assert up(x, condition, noise_scale, skip).shape == expected
