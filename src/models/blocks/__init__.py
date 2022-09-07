fa7504cc82c441b383ba7d73d66e14ebeb1c18a3
gle-bellier <georges.lebellier@gmail.com>
2022-09-07 10:42:41 +0200

    Minor fix

--- src/models/blocks/__init__.py
+++ src/models/blocks/__init__.py
@@ -2,7 +2,7 @@ from .conv_block import ConvBlock
 from .mlp import MLP
 from .rff_block import RFFBlock
 from .gamma_beta import GammaBeta
-from .conditional_emb import ConditionalEmbs
+from .conditional_emb import ConditionEmbedder
 from .resnet_block import ResNetBlock
 from .up_block import UpBlock
 from .down_block import DownBlock
