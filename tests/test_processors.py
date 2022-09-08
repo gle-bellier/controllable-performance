1ca9b3b77a3fdc00d13707496bba3892664caf52
gle-bellier <georges.lebellier@gmail.com>
2022-09-08 13:07:32 +0200

    Update data processors tests

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -7,7 +7,7 @@ from data.dataprocessor.minmax_processor import MinMaxProcessor
 
 def test_minmax_processor_init():
 
-    train_path = "data/contours/expressive/train.pickle"
+    train_path = "data/contours/expressive/extended/train.pickle"
     ddsp_path = "ddsp_violin.ts"
     min_max = MinMaxProcessor(data_range=(-1, 1),
                               train_path=train_path,
@@ -20,17 +20,17 @@ def test_minmax_processor_init():
 def test_minmax_transform(data_range):
 
     # init min max scaler
-    train_path = "data/contours/expressive/train.pickle"
+    train_path = "data/contours/expressive/extended/train.pickle"
     ddsp_path = "ddsp_violin.ts"
     min_max = MinMaxProcessor(data_range=data_range,
                               train_path=train_path,
                               ddsp_path=ddsp_path)
 
     # init train dataset
-    dataset_path = "data/contours/expressive"
+    dataset_path = "data/contours/expressive/extended"
     sample_length = 1024
     batch_size = 32
-    dm = ContoursDataModule(dataset_path, batch_size, sample_length, False)
+    dm = ContoursDataModule(dataset_path, batch_size, 0, sample_length, False)
     dm.setup()
     train = dm.train_dataloader()
 
@@ -44,21 +44,21 @@ def test_minmax_transform(data_range):
 def test_minmax_inverse_transform(data_range):
 
     # init min max scaler
-    train_path = "data/contours/expressive/train.pickle"
+    train_path = "data/contours/expressive/extended/train.pickle"
     ddsp_path = "ddsp_violin.ts"
     min_max = MinMaxProcessor(data_range=data_range,
                               train_path=train_path,
                               ddsp_path=ddsp_path)
 
     # init train dataset
-    dataset_path = "data/contours/expressive"
+    dataset_path = "data/contours/expressive/extended"
     sample_length = 1024
     batch_size = 32
-    dm = ContoursDataModule(dataset_path, batch_size, sample_length, False)
+    dm = ContoursDataModule(dataset_path, batch_size, 0, sample_length, False)
     dm.setup()
     train = dm.train_dataloader()
 
-    for i, sample in enumerate(iter(train), 0):
+    for i, sample in enumerate(train, 0):
         scaled = min_max.transform(sample)
         rec = min_max.inverse_transform(scaled)
 
