092bba8b7751df286da315ee73680923c6b5e7f1
gle-bellier <georges.lebellier@gmail.com>
2022-09-09 19:43:06 +0200

    Adapt methods to training mode

--- src/utils/batch_tools.py
+++ src/utils/batch_tools.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 
 def plot_batch_contours(writer: SummaryWriter, sample: torch.Tensor,
-                        condition: torch.Tensor, epoch_id: int) -> None:
+                        condition: torch.Tensor, epoch_id: int,
+                        mode: str) -> None:
     """Plot random contours of the batch of contours to the writer
 
     Args:
@@ -14,23 +15,26 @@ def plot_batch_contours(writer: SummaryWriter, sample: torch.Tensor,
         sample (torch.Tensor): sample of shape (B C L).
         condition (torch.Tensor): condition of shape (B C L).
         epoch_id (int): id of the epoch.
+        mode (str): train or val or sample (during validation).
     """
 
     # select random element of the batch
+    s = torch.clone(sample).detach()
+    c = torch.clone(condition).detach()
     batch_size = sample.shape[0]
     index = np.random.randint(0, batch_size)
 
-    sample_f0, sample_lo = sample[index].cpu()
-    condition_f0, condition_lo = condition[index].cpu()
+    sample_f0, sample_lo = s[index].cpu()
+    condition_f0, condition_lo = c[index].cpu()
 
     plt.plot(sample_f0, label="sample")
     plt.plot(condition_f0, label="condition")
     plt.legend()
-    writer.add_figure("f0", plt.gcf(), epoch_id)
+    writer.add_figure(f"{mode}/f0", plt.gcf(), epoch_id)
     plt.plot(sample_lo, label="sample")
     plt.plot(condition_lo, label="condition")
     plt.legend()
-    writer.add_figure("lo", plt.gcf(), epoch_id)
+    writer.add_figure(f"{mode}/lo", plt.gcf(), epoch_id)
 
 
 def listen_batch_contours(writer: SummaryWriter, audio: torch.Tensor,
