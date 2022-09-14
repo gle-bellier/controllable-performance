0be55f0f4d6113acfaae287a1da6428381a6b950
gle-bellier <georges.lebellier@gmail.com>
2022-09-14 11:03:56 +0200

    Refactor code into main function

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -4,14 +4,21 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-cli = LightningCLI(pl.LightningModule,
-                   pl.LightningDataModule,
-                   parser_kwargs={
-                       "parser_mode": "omegaconf",
-                       "default_config_files": ["configs/efficient_unet.yaml"]
-                   },
-                   run=False,
-                   subclass_mode_model=True,
-                   subclass_mode_data=True)
 
-cli.trainer.fit(cli.model, cli.datamodule)
+def main():
+    cli = LightningCLI(pl.LightningModule,
+                       pl.LightningDataModule,
+                       parser_kwargs={
+                           "parser_mode": "omegaconf",
+                           "default_config_files":
+                           ["configs/efficient_unet.yaml"]
+                       },
+                       run=False,
+                       subclass_mode_model=True,
+                       subclass_mode_data=True)
+
+    cli.trainer.fit(cli.model, cli.datamodule)
+
+
+if __name__ == "__main__":
+    main()
