c8f5924af1ecba71fcdf4755512c8103d0792b65
gle-bellier <georges.lebellier@gmail.com>
2022-09-26 12:20:10 +0200

    Update evaluation tool

--- src/eval/eval.py
+++ src/eval/eval.py
@@ -1,6 +1,8 @@
 import pytorch_lightning as pl
 import torch
+import torchaudio
 from pytorch_lightning.utilities.cli import LightningCLI
+from einops import rearrange
 
 from data.midi.midi_reader import MidiReader
 
@@ -9,25 +11,54 @@ def main(midi_path: str) -> None:
     cli = LightningCLI(pl.LightningModule,
                        pl.LightningDataModule,
                        parser_kwargs={
-                           "parser_mode":
-                           "omegaconf",
+                           "parser_mode": "omegaconf",
                            "default_config_files":
-                           ["configs/urmp_conditional.yaml"]
+                           ["configs/exd_conditional.yaml"]
                        },
                        run=False,
                        subclass_mode_model=True,
                        subclass_mode_data=True)
 
     state_dict = torch.load(
-        "logs/exd_conditional/version_0/checkpoints/epoch=21299-step=468600.ckpt"
+        "logs/exd_conditional/version_0/checkpoints/epoch=21399-step=470800.ckpt"
     )['state_dict']
-    model = cli.model.load_state_dict(state_dict)
+    cli.model.load_state_dict(state_dict)
+    cli.model.cuda()
+    cli.model.eval()
 
     # get MIDI
     midi_reader = MidiReader()
-    f0, lo, _, _, _ = midi_reader.get_contours(path=midi_path)
+
+    # loop on MIDI samples
+    i_sample = 0
+    for sample in midi_reader.get_contours(path=midi_path):
+
+        f0, lo, _, _, _ = sample
+        f0 = torch.Tensor(f0).reshape(1, 1, -1)
+        lo = torch.Tensor(lo).reshape(1, 1, -1)
+
+        condition = torch.cat([f0, lo], -2).cuda()
+
+        contours, audio = cli.model.sample(condition)
+
+        e_f0, e_lo = contours.split(1, 1)
+        e_f0 = e_f0.cpu().detach().numpy()
+        f0 = f0.cpu().detach().numpy()
+
+        import matplotlib.pyplot as plt
+
+        plt.plot(f0.squeeze())
+        plt.plot(e_f0.squeeze())
+        plt.savefig(f"generated/figures/exd_sample-{i_sample}.png")
+
+        # save audio
+        torchaudio.save(f"generated/audio/exd_sample-{i_sample}.wav",
+                        audio.cpu(), 16000)
+        plt.clf()
+
+        i_sample += 1
 
 
 if __name__ == "__main__":
-    MIDI_FILE = "data/midi/midi_files/jupiter.mid"
+    MIDI_FILE = "data/midi/midi_files/slow.mid"
     main(midi_path=MIDI_FILE)
