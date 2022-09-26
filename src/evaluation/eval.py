c8e942d9d3ae50118ece8b4c06c3a41f3d9da09d
gle-bellier <georges.lebellier@gmail.com>
2022-09-26 15:17:56 +0200

    Update evaluation method and add accuracy of pitch and loudness metric

--- src/evaluation/eval.py
+++ src/evaluation/eval.py
@@ -4,6 +4,7 @@ import torchaudio
 from pytorch_lightning.utilities.cli import LightningCLI
 from einops import rearrange
 
+import matplotlib.pyplot as plt
 from data.midi.midi_reader import MidiReader
 
 from evaluation.metrics.accuracy import Midi_accuracy
@@ -38,22 +39,36 @@ def main(midi_path: str) -> None:
     for sample in midi_reader.get_contours(path=midi_path):
 
         f0, lo, _, _, mask = sample
+
+        # prepare condition
         f0 = torch.Tensor(f0).reshape(1, 1, -1)
         lo = torch.Tensor(lo).reshape(1, 1, -1)
         mask = torch.from_numpy(mask)
-
         condition = torch.cat([f0, lo], -2).cuda()
 
+        # conditional sampling
         contours, audio = cli.model.sample(condition)
 
-        e_f0, e_lo = rearrange(condition, "b c l -> c b l", c=2)
-        e_f0 = e_f0.cpu().detach().numpy()
-        f0 = f0.cpu().detach().numpy()
+        # rearrange and put on CPU
+        contours = contours.cpu().detach().numpy()
+        condition = condition.cpu().detach().numpy()
+        # split generated contours
+        g_f0, g_lo = rearrange(contours, "b c l -> c b l", c=2)
+        # split unexpressive contours (from MIDI)
+        u_f0, u_lo = rearrange(condition, "b c l -> c b l", c=2)
 
-        import matplotlib.pyplot as plt
+        # compute accuracy
 
-        plt.plot(f0.squeeze())
-        plt.plot(e_f0.squeeze())
+        accuracy = metric.forward(u_f0, g_f0, u_lo, g_lo, mask)
+        print(f"Accuracy: {accuracy}")
+
+        # plot contours
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(30, 10))
+        ax1.plot(u_f0.squeeze(), label="midi")
+        ax1.plot(g_f0.squeeze(), label="gen")
+        ax2.plot(u_lo.squeeze(), label="midi")
+        ax2.plot(g_lo.squeeze(), label="gen")
+        plt.legend()
         plt.savefig(f"generated/figures/exd_sample-{i_sample}.png")
 
         # save audio
@@ -65,5 +80,5 @@ def main(midi_path: str) -> None:
 
 
 if __name__ == "__main__":
-    MIDI_FILE = "data/midi/midi_files/slow.mid"
+    MIDI_FILE = "data/midi/midi_files/jupiter.mid"
     main(midi_path=MIDI_FILE)
